676c7890a2735c37ee2b16fe864252132f9ca4c2
rawan-bassam-jazar <rawan.jazar@yahoo.com>
2023-03-23 03:25:06 +0300

    test

---
+++ jsmotoeron.js
@@ -0,0 +1,12 @@
+ var menu=document.getElementById("navlist");
+ 
+ function check() {
+    if(menu.style.display=="none")
+    {
+    menu.style.display="block";
+    }
+    else
+    {
+        menu.style.display="none";
+    }
+ }
